b579a1177d788956036bc4846b41c4e2edc767a6
Tim <timwilburn8@gmail.com>
2018-07-06 16:40:03 -0400

    completed basic schema, hooked up GraphIQL, and started a fake data json-server as outsid e API source

--- schema/schema.js
+++ schema/schema.js
@@ -1,16 +1,37 @@
 //contains all of the knowledge required for telling graphql exactly what the applications data looks like. most importantly what properties each object has, and how exactly how each object is related to eachother
-const graphql = require('graphql');
-const {
-  GraphQLObjectType,
-  GraphQLString,
-  GraphQLInt
-} = graphql;
+const graphql = require("graphql");
+const _ = require("lodash");
+//helpers from the graphql library:
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+
+//this is just the hardcoded user data we will work with rather than database data
+const users = [
+  { id: "23", firstName: "Bill", age: "20" },
+  { id: "47", firstName: "Samantha", age: "21" }
+];
 
 const UserType = new GraphQLObjectType({
-  name: 'User',
+  name: "User",
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt }
   }
-})
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const expressGraphyQL = require("express-graphql"); //compatability layer for the two to work together
+const expressGraphQL = require("express-graphql"); //compatability layer for the two to work together
+const schema = require('./schema/schema');
 
 const app = express();
 
 app.use(
   "/graphql",
-  expressGraphyQL({
+  expressGraphQL({
+    schema, //es6 magic - key and value are the same so the single name = schema: schema,
     //can break the uppercase rule in these circumstances
     graphiql: true //dev tool that helps us use the premade application
   })
